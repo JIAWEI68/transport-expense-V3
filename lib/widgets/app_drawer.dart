550e1e259ac84d12d4fdf304b83881aa280d6c8f
JIAWEI68 <83011056+JIAWEI68@users.noreply.github.com>
2022-07-12 11:58:54 +0800

    firebase auth finished

--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:transport_expense_tracker/main.dart';
 import 'package:transport_expense_tracker/screens/expense_list_screen.dart';
+import 'package:transport_expense_tracker/services/auth_service.dart';
 
 class AppDrawer extends StatelessWidget {
-
-
   @override
   Widget build(BuildContext context) {
+    AuthService authService = AuthService();
     return Drawer(
       child: Column(children: [
         AppBar(
-          title: Text("Hello Friend!"),
+          title: FittedBox(
+            child: Text('Hello' + authService.getCurrentUser()!.email! + '!'),
+          ),
           automaticallyImplyLeading: false,
         ),
         ListTile(
@@ -23,8 +25,8 @@ class AppDrawer extends StatelessWidget {
         ListTile(
           leading: Icon(Icons.monetization_on),
           title: Text('My Expenses'),
-          onTap: () =>
-              Navigator.of(context).pushReplacementNamed(ExpenseListScreen.routeName),
+          onTap: () => Navigator.of(context)
+              .pushReplacementNamed(ExpenseListScreen.routeName),
         ),
         Divider(height: 3, color: Colors.blueGrey),
       ]),
